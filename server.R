74dc7e819ac934baefe9a801e83b8470991cb74f
pc <nguyenduclam0605@gmail.com>
2020-01-29 00:27:30 +0700

    Shiny_TVDI

---
+++ server.R
@@ -0,0 +1,56 @@
+library(leaflet)
+library(shiny)
+library(shinydashboard)
+library(rgdal)
+library(raster)
+
+ras <- stack(list.files("data", full.names=T, pattern = "*.tif$"))
+
+function (input, output, sesion){
+  xBase <- (extent(ras)[2] + extent(ras)[1]) / 2
+  yBase <- (extent(ras)[4] + extent(ras)[3]) / 2
+  sp <- SpatialPoints(data.frame(xBase, yBase))
+  crs(sp) <- crs(ras)
+  sp <- spTransform(sp, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+  
+  plot(ras[[1]])
+  points(sp)
+  output$rasPlot <- renderPlot({
+    plot(ras[[input$layer]])
+  }, height = 400)
+  
+  output$rasProj <- renderText(projection(ras))
+  output$rasRes <- renderText(res(ras))
+  output$rasDim <- renderText(dim(ras))
+  output$rasNlayers <- renderText(nlayers(ras))
+  output$cellnumber <- renderText(round(Coords(), 4))
+  output$rasvalue <- renderText(value())
+  
+  Coords <- reactive({
+    req(input$rasPlot_click$x)
+    c(input$rasPlot_click$x, input$rasPlot_click$y)
+  })
+  
+  output$df <- renderTable({
+    valdf <- data.frame("Layer" = names(ras), "Value" = unlist(value())[1,])
+    colnames(valdf) <- c("Name", "Value")
+    rownames(valdf) <- 1:nlayers(ras)
+    valdf 
+  })
+  
+  value  <- eventReactive(input$rasPlot_click$x,{
+    extract(ras,cellFromXY(ras, Coords()))
+  })
+  
+  output$Map <- renderLeaflet({
+    leaflet() %>% 
+      setView(lng = sp@coords[1], lat = sp@coords[2], zoom = 6) %>% 
+      addProviderTiles("Esri.WorldImagery") 
+  })
+  
+  #output$Map <- renderLeaflet({
+  #leaflet() %>%
+  #  addProviderTiles(providers$Esri.WorldImagery) %>%
+  #  setView(lng = 108.384, lat = 13.794, zoom = 6)
+  #})
+}
---
+++ ui.R
@@ -0,0 +1,56 @@
+library(leaflet)
+library(shiny)
+library(shinydashboard)
+library(rgdal)
+library(raster)
+
+dashboardPage(
+  dashboardHeader(
+    title = "R Gis - Raster Time Series Analysis",
+    titleWidth = 450
+  ),
+  dashboardSidebar(
+    h4("Filter:"),
+    checkboxInput("filterCheckSav", "Savitzky-Golay", value = FALSE)
+  ),
+
+  dashboardBody(
+    fluidRow(
+      tabBox(
+        title = "Interactive Image Analysis", id = "tabset",
+        tabPanel("Raster",
+                 plotOutput("rasPlot", click = "rasPlot_click"),
+                 #fileInput("ras", h3("Raster Input"), multiple = T),
+                 sliderInput("layer", 
+                             "Plot Timestep", 
+                             min = 1, 
+                             max = nlayers(ras), 1, 
+                             width="100%"),
+                 dateRangeInput("dates", h3("Date range"))),
+        tabPanel("Basemap", leafletOutput("Map", width = "100%", height = 700))
+      ),
+      
+      box(
+        title = "Properties", status = "info", solidHeader = TRUE,
+        HTML("<b>Layers:</b>"),
+        textOutput("rasNlayers"),
+        HTML("<b>Resolution:</b>"),
+        textOutput("rasRes"),
+        HTML("<b>Projection:</b>"),
+        textOutput("rasProj"),
+        h4("Selected Coordinates"),
+        textOutput("cellnumber")
+      ),
+      
+      box(
+        title = "Time Series", status = "success", solidHeader = TRUE,
+        plotOutput("TSplot")
+      ),
+      
+      box(
+        title = "Values", status = "info", solidHeader = TRUE,
+        tableOutput("df")
+      )
+    )
+  )
+)
